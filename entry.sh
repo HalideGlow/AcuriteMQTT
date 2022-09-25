09f4e1e998812bd82614a95bf612260a1e900368
HalideGlow <test@example.local>
2022-09-25 21:28:56 +0000

    Update PROTOCOLS.md, entry.sh

--- entry.sh
+++ entry.sh
@@ -2,12 +2,9 @@
 
 # Exit immediately if a command exits with a non-zero status:
 set -e
-
-
 export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 export LD_LIBRARY_PATH=/usr/local/lib64
-
 
 # Start the listener and enter an endless loop
 echo "Starting RTL_433 with parameters:"
@@ -25,7 +22,5 @@ echo "UNITS =" $UNITS
 echo "DISCOVERY_PREFIX =" $DISCOVERY_PREFIX
 echo "DISCOVERY_INTERVAL =" $DISCOVERY_INTERVAL
 echo "DEBUG =" $DEBUG
-
-
 
 rtl_433  $PROTOCOL -C $UNITS  -F mqtt://$MQTT_HOST:$MQTT_PORT,user=$MQTT_USERNAME,pass=$MQTT_PASSWORD,retain=$MQTT_RETAIN,events=$MQTT_TOPIC/events,states=$MQTT_TOPIC/states,devices=$MQTT_TOPIC[/model][/id][/channel:0]  -M time:tz:local -M protocol -M level | /scripts/rtl_433_mqtt_hass.py
